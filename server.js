7e53e9c41125b078a07eb3724a1dcfb6fa7ccdb2
Ron <ron.douglas@canopytax.com>
2021-10-02 15:05:17 -0600

    nodemon, env, and json support

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import index from "./routes/index.js";
+
+dotenv.config();
 
 const port = process.env.PORT || 4001;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 app.use(index);
 
 const server = http.createServer(app);
